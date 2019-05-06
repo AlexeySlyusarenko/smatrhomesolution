1172ee9610e2b40fab1221dddbfead274bb3186b
Alexey Slyusarenko <slyusarenko.a.n@gmail.com>
2019-05-06 21:16:24 +0300

    Test view

--- js/index.js
+++ js/index.js
@@ -1,4 +1,40 @@
-import init from '/js/func/init.js';
-import template from '/js/template/template.js';
+class Button {
+    constructor(button) {
+        this.elem = button;
 
-init(document.body, template);
+        // Handler event
+        this.handleEvent = function(event) {
+            switch(event.type) {
+            case 'touchstart':
+
+            break;
+            case 'touchend':
+
+            break;
+            case 'touchmove':
+
+            break;    
+            }
+        };
+    }
+    //
+    enableHandlers() {
+        this.elem.addEventListener('touchstart', this, false);
+        this.elem.addEventListener('touchend', this, false);
+    }
+    disableHandlers() {
+        this.elem.removeEventListener('touchstart', this, false);
+        this.elem.removeEventListener('touchend', this, false);
+    }
+}
+
+let buttonObjArr = [],
+    pageElem = document.querySelector('.page'),
+    pageTrapElem = document.querySelector('.page__trap'),
+    pageControlElem = document.querySelector('.page__control'),
+    pageNavElem = document.querySelectorAll('.page__nav'),
+    buttonElemArr = document.querySelectorAll('.button');
+
+for (let i = 0; i < buttonElemArr.length; i++) {
+    buttonObjArr[i] = new Button(buttonElemArr[i]);
+}
