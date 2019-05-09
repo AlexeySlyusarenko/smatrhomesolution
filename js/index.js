3f91a399b4a3b33fa97a395ec94c002db40ae3fe
Alexey Slyusarenko <slyusarenko.a.n@gmail.com>
2019-05-09 20:40:23 +0300

    Test view

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 class Button {
-    constructor(button) {
-        this.elem = button;
+    constructor(buttonElem, pageElem) {
+        this.elem = buttonElem;
+        this.pageElem = pageElem;
 
         // Handler event
-        this.handleEvent = function(event) {
-            switch(event.type) {
-            case 'touchstart':
+        // this.handleEvent = function(event) {
+        //     switch(event.type) {
+        //         case 'touchstart':
 
-            break;
-            case 'touchend':
+        //         break;
+        //         case 'touchend':
 
-            break;
-            case 'touchmove':
+        //         break;
+        //         case 'touchmove':
 
-            break;    
-            }
-        };
+        //         break;    
+        //     }
+        // };
     }
     //
     enableHandlers() {
@@ -28,13 +29,65 @@ class Button {
     }
 }
 
+class ButtonNav extends Button {
+    constructor(buttonElem, pageElem) {
+        super(buttonElem, pageElem);
+
+        // Handler event
+        this.handleEvent = function(event) {
+            switch(event.type) {
+                case 'touchstart':
+
+                break;
+                case 'touchend':
+
+                break;
+                case 'touchmove':
+
+                break;    
+            }
+        };
+    }
+}
+
+class ButtonShowNav extends Button {
+    constructor(buttonElem, pageElem) {
+        super(buttonElem, pageElem);
+     
+        // Handler event
+        this.handleEvent = function(event) {
+            switch(event.type) {
+                case 'touchstart':
+
+                break;
+                case 'touchend':
+                    this.pageElem.classList.toggle('page--nav-show');
+                break;
+                case 'touchmove':
+
+                break;    
+            }
+        };
+
+        this.enableHandlers();
+    }
+} 
+
 let buttonObjArr = [],
     pageElem = document.querySelector('.page'),
     pageTrapElem = document.querySelector('.page__trap'),
     pageControlElem = document.querySelector('.page__control'),
-    pageNavElem = document.querySelectorAll('.page__nav'),
-    buttonElemArr = document.querySelectorAll('.button');
+    pageNavElemArr = document.querySelectorAll('.page__nav');
 
-for (let i = 0; i < buttonElemArr.length; i++) {
-    buttonObjArr[i] = new Button(buttonElemArr[i]);
+let buttonControlElemArr = pageControlElem.querySelectorAll('.button');
+
+for (let i = 0; i < pageNavElemArr.length; i++) {
+    buttonNavElemArr = pageNavElemArr[i].querySelectorAll('.button');
+}
+
+for (let i = 0; i < buttonNavElemArr.length; i++) {
+    if (buttonNavElemArr[i].classList.contains('button--show-nav')) {
+        buttonShowNavObj = new ButtonShowNav(buttonNavElemArr[i], pageElem);
+    }
+    buttonNavElemArr[i] = new ButtonNav(buttonNavElemArr[i], pageElem);
 }
