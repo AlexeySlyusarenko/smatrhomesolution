6a25a87f476534f4ca41cfb4dd7d4ae9310f7598
Alexey Slyusarenko <slyusarenko.a.n@gmail.com>
2019-05-26 07:26:00 +0300

    Test view

--- js/index.js
+++ js/index.js
@@ -27,8 +27,9 @@ class Page {
     //
     enableHandlers() {
         this.elem.onresize = () => {
-            this.getSizeAndOrientation();
+            this.getSizeAndOrientationWidow();
             this.setStyleSize();
+            this.navObj.setElemStyle();
             if (this.setTimeoutId) clearTimeout(this.setTimeoutId);
             this.setTimeoutId = setTimeout(() => {
                 this.getElemSize();
@@ -37,9 +38,9 @@ class Page {
         };
     }
     //
-    getSizeAndOrientation() {
-        this.width = this.elem.getBoundingClientRect().width;
-        this.height = this.elem.getBoundingClientRect().height;
+    getSizeAndOrientationWidow() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
         if (this.width > this.height) {
             this.maxSize = this.width;
             this.minSize = this.height;
@@ -81,7 +82,7 @@ class Page {
         }
     }
     init() {
-        this.getSizeAndOrientation();
+        this.getSizeAndOrientationWidow();
         this.setStyleSize();
         this.getElemSize();
         this.setElemStyleSize();
@@ -93,6 +94,12 @@ class Nav {
         this.elem = navElem;
         this.pageElem = pageElem;
 
+        this.listElemArr = this.elem.querySelectorAll('.nav__list');
+        this.itemElemArr = [];
+        for (let i = 0; i < this.listElemArr.length; i++) {
+            this.itemElemArr[i] = this.listElemArr[i].querySelectorAll('.nav__item')
+        }
+
         this.buttonElemArr = this.elem.querySelectorAll('.button');
         this.buttonObjArr = [];
 
@@ -102,6 +109,15 @@ class Nav {
             }
             this.buttonObjArr[i] = new ButtonNav(this.buttonElemArr[i], this.pageElem);
         }
+
+        this.init();
+    }
+    setElemStyle() {
+        // this.pageElem.orientation
+        this.pageElem.style.setProperty('--button-number-in-main-nav-row', 7);
+    }
+    init() {
+        this.setElemStyle();
     }
 }
 
